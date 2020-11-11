ab334c4eb8d551307f569fc82e938449ca0ed7a7
Reski Junaidi Shalat <reskijunaedisalat1@gmail.com>
2020-11-12 04:59:45 +0700

    New Update

---
+++ handlers/xp.js
@@ -0,0 +1,47 @@
+const db = require("quick.db")
+const Discord = require("discord.js")
+class Util {
+  static getLevel(xp, extra = false) {
+  let level = 0;
+    
+    //WHILE LOOP
+    while(xp >= Util.getLevelxp(level)) {
+      xp -= Util.getLevelxp(level);
+      level++
+    }
+    if(extra) return [level, xp];
+    else return level;
+    }
+
+  
+  static getLevelxp(level) {
+    return 5 * Math.pow(level, 2) + 50 * level + 100;
+  }
+  
+  static getInfo(exp) {
+    let [level, remxp] = Util.getLevel(exp, true);
+    let levelxp = Util.getLevelxp(level);
+    
+    return { level, remxp, levelxp}
+  }
+  
+  static addexp(message) {
+    let toadd = Math.floor(Math.random() * 3 + 3);
+    let oldxp = db.get(`xp_${message.author.id}_${message.guild.id}`)
+    let oldlvl = Util.getLevel(oldxp)
+    let newxp = oldxp + toadd;
+    let newlvl = Util.getLevel(newxp);
+    
+    let embedAnnouncement = new Discord.MessageEmbed()
+    .setColor("BLUE")
+    .setDescription(`${message.author}, You just reached level ${newlvl}`);
+    
+    if(newlvl > oldlvl) 
+      
+    message.channel.send(embedAnnouncement)
+    db.add(`xp_${message.author.id}_${message.guild.id}`, toadd)
+    
+  }
+}
+
+module.exports = Util;
